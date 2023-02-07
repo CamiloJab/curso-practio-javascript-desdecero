85b3c7c565f4f86314e14c41734e8e4a3ac7449f
cristhyan <cristhyan.camilo@gmailcom>
2023-02-07 10:30:26 -0500

    se adecuo el boton de carrito de compras, mobile menu y login y logout

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isDesktopMenuClosed = aside.classList.toggle('inactive');
+
+    if(!isDesktopMenuClosed){
+        aside.classList.toggle('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.toggle('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.toggle('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.toggle('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
 }
